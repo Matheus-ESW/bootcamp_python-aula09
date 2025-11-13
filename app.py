a0166e384b5d203103b0cb015e55b70abca7fb3f
Matheus Ramos <matheussoaresramos@gmail.com>
2025-11-13 20:07:27 -0300

    Exemplo loguru!

---
+++ app.py
@@ -0,0 +1,10 @@
+from loguru import logger
+
+# Mensagens de log de diferentes níveis
+logger.debug("Isso é uma mensagem de debug")
+logger.info("Isso é uma mensagem informativa")
+logger.warning("Isso é um aviso")
+logger.error("Isso é um erro")
+logger.critical("Isso é crítico")
+
+# A saída será exibida no console

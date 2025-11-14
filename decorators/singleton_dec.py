03b23a520a75a4c89c7c22dd8c8ddd602645942c
Matheus Ramos <matheussoaresramos@gmail.com>
2025-11-14 16:05:25 -0300

    Ajuste decorators!

---
+++ decorators/singleton_dec.py
@@ -0,0 +1,12 @@
+def singleton(cls):
+    instances = {}
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return get_instance
+
+@singleton
+class DatabaseConnection:
+    def __init__(self):
+        print("Inicializando uma nova instância da conexão com o banco de dados.")
